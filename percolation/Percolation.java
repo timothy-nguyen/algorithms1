f3511ce884204666c9563461b1a771db28dc72a5
Timothy Nguyen <timothy.nguyen33@gmail.com>
2023-07-29 08:01:08 +1000

    Added percolation stats. Submission fail

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -4,21 +4,23 @@
  *  Last modified:     1/1/2019
  **************************************************************************** */
 
-import edu.princeton.cs.algs4.UF;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
     private int[][] grid;  // n * n grid
-    private UF tree;       // union-find object size n * n
+    private WeightedQuickUnionUF tree;       // union-find object size n * n
     private int n;         // n
     private int countOpen; // number of open sites
-    private int countFull; // number of full sites
 
     // creates n-by-n grid, with all sites initially blocked
-    public Percolation(int n) throws IllegalArgumentException {
+    public Percolation(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException();
+        }
+
         this.n = n;
         countOpen = 0;
-        countFull = 0;
-        tree = new UF(n * n);
+        tree = new WeightedQuickUnionUF(n * n);
         grid = new int[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
@@ -28,7 +30,7 @@ public class Percolation {
     }
 
     // opens the site (row, col) if it is not open already
-    public void open(int row, int col) throws IllegalArgumentException {
+    public void open(int row, int col) {
         try {
             // If already open then do nothing
             if (isOpen(row, col)) {
@@ -38,6 +40,7 @@ public class Percolation {
             // Else open site
             int rowIdx = row - 1; // revert back to zero indexing
             int colIdx = col - 1;
+            // System.out.printf("rowIdx: %d. colIdx: %d\n", rowIdx, colIdx);
             grid[rowIdx][colIdx] = 1;
             countOpen += 1;
 
@@ -45,37 +48,24 @@ public class Percolation {
             int[] rowNbr = { -1, 0, 0, 1 };
             int[] colNbr = { 0, -1, 1, 0 };
             for (int i = 0; i < rowNbr.length; i++) {
-                // System.out.format("i: %d\n", i);
                 if (isOpen(rowIdx + rowNbr[i] + 1, colIdx + colNbr[i] + 1)) {
-                    // System.out.println("isOpen");
-                    // System.out.println(rowIdx + rowNbr[i]);
-                    // System.out.println(colIdx + colNbr[i]);
-
                     // p is current {row, col}. q is adjacent site
                     // need to reference p and q in the flattened tree array
-                    // System.out.println("Points");
                     int p = rowIdx * n + colIdx;
                     int q = (rowIdx + rowNbr[i]) * n + (colIdx + colNbr[i]);
 
-                    // System.out.println(p);
-                    // System.out.println(q);
-
                     tree.union(p, q);
-                    // System.out.println("Root:");
-                    // System.out.println(tree.find(p));
-                    // System.out.println(tree.find(q));
                 }
             }
         }
-        catch (IllegalArgumentException e) {
+        catch (IllegalArgumentException | ArrayIndexOutOfBoundsException e) {
             return;
         }
     }
 
     // is the site (row, col) open?
     // * throw  IllegalArgumentException if input is outside [1, n]
-    public boolean isOpen(int row, int col) throws IllegalArgumentException,
-                                                   ArrayIndexOutOfBoundsException {
+    public boolean isOpen(int row, int col) {
         try {
             if (grid[row - 1][col - 1] == 1) return true;
             return false;
@@ -86,11 +76,16 @@ public class Percolation {
     }
 
     // is the site (row, col) full?
-    // is full if the site is connected to the top (i.e. root is 0 to n - 1)
-    public boolean isFull(int row, int col) throws IllegalArgumentException,
-                                                   ArrayIndexOutOfBoundsException {
+    // is full if the site is connected to the top (i.e. root is 0 to n - 1) [does not work if root is in middle of the grid]
+    // if full if site is connected to a node at the top
+    public boolean isFull(int row, int col) {
         try {
-            if (tree.find((row - 1) * n + (col - 1)) < n && isOpen(row, col)) return true;
+            // System.out.printf("Tree index: %d\n", (row - 1) * n + (col - 1));
+            // System.out.printf("Tree root: %d\n", tree.find((row - 1) * n + (col - 1)));
+            for (int j = 0; j < n; j++) {
+                if (tree.find((row - 1) * n + (col - 1)) == tree.find(j) && isOpen(row, col))
+                    return true;
+            }
             return false;
         }
         catch (IllegalArgumentException | ArrayIndexOutOfBoundsException e) {
@@ -104,32 +99,15 @@ public class Percolation {
     }
 
     // does the system percolate?
-    // system percolates if any bottom node is connected to any top node
-    // or if we keep track of isFull then if bottom is full then it percolates
+    // if bottom is full then it percolates
     public boolean percolates() {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; i < n; j++) {
-                int bottomIdx = (n - 1) * n + i;
-                if (tree.find(bottomIdx) == tree.find(j)) return true;
-            }
+        for (int j = 1; j <= n; j++) {
+            if (isFull(n, j)) return true;
         }
         return false;
     }
 
     // test client (optional)
     public static void main(String[] args) {
-        Percolation perc = new Percolation(4);
-        perc.open(1, 1);
-        perc.open(1, 2);
-        perc.open(2, 3);
-        perc.open(4, 4);
-        perc.open(3, 4);
-        perc.open(3, 1);
-        perc.open(4, 1);
-        perc.open(3, 2);
-        // System.out.println(perc.tree.find(6));
-        // System.out.println(perc.tree.find(0));
-        // System.out.println(perc.tree.find(15));
-        PercolationVisualizer.draw(perc, 4);
     }
 }
---
+++ percolation/PercolationStats.java
@@ -0,0 +1,80 @@
+/* *****************************************************************************
+ *  Name:              Alan Turing
+ *  Coursera User ID:  123456
+ *  Last modified:     1/1/2019
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+public class PercolationStats {
+    private int trials;
+    private double[] results;
+    private double CONFIDENCE_95 = 1.96;
+
+    // perform independent trials on an n-by-n grid
+    public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        this.trials = trials;
+        results = new double[trials];
+        for (int i = 0; i < trials; i++) {
+            Percolation perc = new Percolation(n);
+            while (!perc.percolates()) {
+                int row = StdRandom.uniformInt(1, n + 1);
+                int col = StdRandom.uniformInt(1, n + 1);
+                // System.out.printf("row: %d. col: %d\n", row, col);
+                perc.open(row, col);
+            }
+            // System.out.println("Trial " + i + " complete\n");
+            double countOpen = 0;
+            for (int j = 1; j < n + 1; j++) {
+                for (int k = 1; k < n + 1; k++) {
+                    if (perc.isOpen(j, k)) countOpen += 1;
+                }
+            }
+            results[i] = countOpen / (n * n);
+        }
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        return StdStats.mean(results);
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        // return StdStats.var(results);
+        double ssq = 0;
+        for (int i = 0; i < results.length; i++) {
+            ssq += (results[i] - mean()) * (results[i] - mean());
+        }
+        return ssq / (trials - 1);
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLo() {
+        return mean() - (CONFIDENCE_95 * Math.sqrt(stddev()) / Math.sqrt(trials));
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+        return mean() + (CONFIDENCE_95 * Math.sqrt(stddev()) / Math.sqrt(trials));
+    }
+
+    // test client (see below)
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
+        // System.out.printf("n: %d. trials: %d\n", n, trials);
+        PercolationStats percStats = new PercolationStats(n, trials);
+        StdOut.printf("mean                    = %f\n", percStats.mean());
+        StdOut.printf("stddev                  = %f\n", percStats.stddev());
+        StdOut.println("95% confidence interval = [" + percStats.confidenceLo() + ", "
+                               + percStats.confidenceHi() + "]");
+    }
+
+}
